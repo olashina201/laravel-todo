cb1586eaf7fa0758a8ca49af1d4ba99de95be1bd
olashina201 <olashina201@gmail.com>
2022-07-13 16:35:12 +0100

    interface

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,27 @@
             </form>
         </div>
         <hr />
-        
+        <div class="mt-2 ">
+          <div class="py-4 flex items-center border-gray-300 px-3">
+            <div class="flex-1 pr-8">
+              <h3 class="text-lg font-semibold">The title</h3>
+              <p class="text-gray-500">The description</p>
+            </div>
+
+            <div class="flex space-x-3">
+              <button class="py-2 px-2 bg-green-500 text-white rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                </svg>
+              </button>
+              <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>	
       </div>
     </body>
 </html>
